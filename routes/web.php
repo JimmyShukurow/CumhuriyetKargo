17b89c8a745f969ac994c83fff693003a0b8f1e6
erhanask <erhanasik8@gmail.com>
2021-09-25 09:05:17 +0300

    who is who done

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,7 @@ Route::group(['middleware' => ['CheckAuth', 'CheckStatus']], function () {
     Route::group(['prefix' => 'WhoIsWho', 'as' => 'whois.'], function () {
         Route::get('', [WhoIsController::class, 'index'])->name('index');
         Route::get('GetUsers', [WhoIsController::class, 'getUsers'])->name('getUsers');
+        Route::post('GetUserInfo', [WhoIsController::class, 'userInfo']);
     });
 
 
