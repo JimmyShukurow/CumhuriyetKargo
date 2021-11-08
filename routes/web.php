ec08bdb6ec31c589e0ffd07462596383e57e6c0b
nurullahguc <60353265+nurullahguc@users.noreply.github.com>
2021-11-08 13:48:40 +0300

    some

--- routes/web.php
+++ routes/web.php
@@ -140,14 +140,12 @@ Route::group(['middleware' => ['CheckAuth', 'CheckStatus']], function () {
     });
 
     Route::group(['prefix' => 'CargoBags', 'as' => 'cargoBags.'], function () {
-
         Route::group(['prefix' => 'Agency', 'as' => 'agency'], function () {
             Route::get('/', [CargoBagsController::class, 'agencyIndex'])->name('Index');
             Route::get('/GetCargoBags', [CargoBagsController::class, 'getCargoBags'])->name('GetCargoBags');
             Route::post('/CreateBag', [CargoBagsController::class, 'createBag'])->name('CreateBag');
             Route::post('/GetBagInfo', [CargoBagsController::class, 'getBagInfo'])->name('GetBagInfo');
         });
-
     });
 
     Route::group(['prefix' => 'Operation'], function () {
