4225fd0f8dfad9eadf6759e91e639330c086760c
Jemshit Shukurov <j.shukurow@cumhuriyetkargo.com.tr>
2022-02-02 16:10:06 +0300

    cargo detaylar eklendi

--- app/Http/Resources/CargoBagDetailsResource.php
+++ app/Http/Resources/CargoBagDetailsResource.php
@@ -12,8 +12,15 @@ class CargoBagDetailsResource extends JsonResource
         return [
             'cargo_id' => $this->id,
             'ctn' => $this->tracking_no,
-            'part_no' => $this->partDetails->pluck('part_no'),
-
+            'part_no' => 1,
+            'user_id' => $this->creator_user_id,
+            'agency_code' => $this->creator_agency_code,
+            'deleted_at' => $this->deleted_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'receiver_name' => $this->receiver_name,
+            'receiver_city' => $this->receiver_city,
+            'recevier_district' => $this->receiver_district,
         ];
     }
 }
