944f06854ecaf9ce1b59a71967425e1ece178b3e
Jemshit Shukurov <j.shukurow@cumhuriyetkargo.com.tr>
2022-02-10 10:44:46 +0300

    time format is fixed in torba details

--- app/Models/Cargoes.php
+++ app/Models/Cargoes.php
@@ -183,4 +183,9 @@ class Cargoes extends Model
         return $this->belongsToMany(CargoBags::class, 'cargo_bag_details', 'cargo_id', 'bag_id')->wherePivot('is_inside', '1');
     }
 
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+        'deleted_at' => 'datetime:Y-m-d H:m:s',
+    ];
 }
