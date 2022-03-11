f5ba3db62db44df40103f4b6a74f8bbf562c7030
Niko JR <bbcn419a@gmail.com>
2022-03-11 16:16:20 +0300

    Agencies index table developed

--- public/backend/assets/scripts/agencies/index.js
+++ public/backend/assets/scripts/agencies/index.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
             {
                 extend: 'excelHtml5',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
                 },
                 title: "CKGSis-Acenteler"
             },
