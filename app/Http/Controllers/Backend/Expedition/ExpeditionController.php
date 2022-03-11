db87aa3824569012d4edb91bbdfe00c9c4c20010
JimmyShukurow <jimishukurow@gmail.com>
2022-03-11 20:22:59 +0300

    Expedition  load cargo bitti sayilir

--- app/Http/Controllers/Backend/Expedition/ExpeditionController.php
+++ app/Http/Controllers/Backend/Expedition/ExpeditionController.php
@@ -9,7 +9,6 @@ use App\Models\Agencies;
 use App\Models\TransshipmentCenters;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class ExpeditionController extends Controller
 {
--- app/Http/Requests/Expedition/LoadCargoToExpeditionRequest.php
+++ app/Http/Requests/Expedition/LoadCargoToExpeditionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Expedition;
 
+use App\Rules\CheckCargoInExpedition;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoadCargoToExpeditionRequest extends FormRequest
@@ -15,14 +16,23 @@ class LoadCargoToExpeditionRequest extends FormRequest
 
     public function rules()
     {
+        $cargo_id = $this->request->get('cargo_id');
         return [
-            'expedition_id' => 'required',
+            'expedition_id' => 'required|exists:expeditions,id',
             'cargo_id' => 'required',
-            'part_no' => 'required',
+            'part_no' => ['required', new CheckCargoInExpedition($cargo_id)],
             'user_id' => 'required',
             'unloading_user_id' => 'nullable',
             'unloading_at' => 'nullable',
 
         ];
     }
+    public function messages()
+    {
+        return [
+            'expedition_id.exists' => 'Bu Sefer Mevcut Değil',
+        ];
+    }
+
+
 }
