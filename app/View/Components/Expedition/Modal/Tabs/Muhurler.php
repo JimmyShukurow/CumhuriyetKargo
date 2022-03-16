a6998caf6810e111515012bd20f11c72f5cd35b4
Jemshit Shukurov <j.shukurow@cumhuriyetkargo.com.tr>
2022-03-16 18:34:14 +0300

    Expedition Modal is going on

---
+++ app/View/Components/Expedition/Modal/Tabs/Muhurler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\View\Components\Expedition\Modal\Tabs;
+
+use Illuminate\View\Component;
+
+class Muhurler extends Component
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function render()
+    {
+        return view('components..expedition.modal.tabs.muhurler');
+    }
+}
--- resources/views/backend/expedition/outgoing/outgoing_expeditions.blade.php
+++ resources/views/backend/expedition/outgoing/outgoing_expeditions.blade.php
@@ -170,8 +170,9 @@
     <script src="/backend/assets/scripts/select2.js"></script>
     <script src="/backend/assets/scripts/city-districts-point.js"></script>
     <script src="/backend/assets/scripts/expeditions/outgoing-expedition.js"></script>
+    <script src="/backend/assets/scripts/expeditions/outgoing-expedition-details.js"></script>
 @endsection
 
 @section('modals')
-
+    @include('backend.expedition.outgoing.outgoing_expeditions_modal')
 @endsection
