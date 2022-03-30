571bc018a0b7b6bea5f50e4c5bf268d0d97899e5
Jemshit Shukurov <j.shukurow@cumhuriyetkargo.com.tr>
2022-03-30 13:48:09 +0300

    ok

--- app/Http/Controllers/CKG_Mobile/ExpeditionCargoMobileController.php
+++ app/Http/Controllers/CKG_Mobile/ExpeditionCargoMobileController.php
@@ -42,17 +42,17 @@ class ExpeditionCargoMobileController extends Controller
 
         $cargo = Cargoes::where('tracking_no', $ctn[0])->first();
 
-        #check by agencies locallocations from route id
-        $route = ExpeditionRoute::find($request->route_id);
-        if ($route->branch_type == 'Acente') {
-            $check = CheckCargoRouteAgencyLocationMatchAction::run($route->branch, $cargo);
-            if (! $check) {
-                return [
-                    'status' => 0,
-                    'message' => 'Bu kargonun varış şubesi '. $cargo->arrival_branch_name .' ŞUBE olduğundan'. $route->branch_details .' ŞUBEYE yükleme yapamazsınız.!'
-                ];
-            }
-        }
+//        #check by agencies locallocations from route id
+//        $route = ExpeditionRoute::find($request->route_id);
+//        if ($route->branch_type == 'Acente') {
+//            $check = CheckCargoRouteAgencyLocationMatchAction::run($route->branch, $cargo);
+//            if (! $check) {
+//                return [
+//                    'status' => 0,
+//                    'message' => 'Bu kargonun varış şubesi '. $cargo->arrival_branch_name .' ŞUBE olduğundan'. $route->branch_details .' ŞUBEYE yükleme yapamazsınız.!'
+//                ];
+//            }
+//        }
 
         if ($cargo && ($cargo->cargo_type == 'Dosya' || $cargo->cargo_type == 'Mi'))
             return response()->json([
