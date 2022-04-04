7c15fa2a27ad9758c6c47024689a967c44700483
Jemshit Shukurov <j.shukurow@cumhuriyetkargo.com.tr>
2022-04-04 10:51:04 +0300

    route_id nullable

--- app/Http/Controllers/CKG_Mobile/ExpeditionCargoMobileController.php
+++ app/Http/Controllers/CKG_Mobile/ExpeditionCargoMobileController.php
@@ -67,7 +67,7 @@ class ExpeditionCargoMobileController extends Controller
                 $fields['cargo_id'] = $cargo->cargo->id;
                 $fields['part_no'] = $cargo->part_no;
                 $fields['user_id'] = $user_id;
-                $fields['expedition_route_id'] = $validated->route_id;
+//                $fields['expedition_route_id'] = $validated->route_id;
                 CargoExpeditionMovementAction::run($cargo->cargo->tracking_no, $cargo->cargo, $user_id, $cargo->part_no, rand(4, 10), 1, 'load_cargo_expedition', $expedition->car->plaka);
                 LoadCargoToExpeditionAction::run($fields);
 
@@ -82,17 +82,17 @@ class ExpeditionCargoMobileController extends Controller
                 'message' => 'Bu kargonun taşımasını Cumhuriyet Kargo yapmadığından yükleme işlemi gerçekletiremezsiniz!',
             ]);
 
-        #check by agencies locallocations from route id
-        $route = ExpeditionRoute::find($request->route_id);
-        if ($route->branch_type == 'Acente') {
-            $check = CheckCargoRouteAgencyLocationMatchAction::run($route->branch, $cargo);
-            if (! $check) {
-                return [
-                    'status' => 0,
-                    'message' => 'Bu kargonun varış şubesi '. $cargo->arrivalBranchAgency->agency_name .' ŞUBE olduğundan '. $route->branch_details .'YE yükleme yapamazsınız.!'
-                ];
-            }
-        }
+//        #check by agencies locallocations from route id
+//        $route = ExpeditionRoute::find($request->route_id);
+//        if ($route->branch_type == 'Acente') {
+//            $check = CheckCargoRouteAgencyLocationMatchAction::run($route->branch, $cargo);
+//            if (! $check) {
+//                return [
+//                    'status' => 0,
+//                    'message' => 'Bu kargonun varış şubesi '. $cargo->arrivalBranchAgency->agency_name .' ŞUBE olduğundan '. $route->branch_details .'YE yükleme yapamazsınız.!'
+//                ];
+//            }
+//        }
 
 
         $user_id = Auth::id();
@@ -100,7 +100,7 @@ class ExpeditionCargoMobileController extends Controller
         $fields['cargo_id'] = $cargo->id;
         $fields['part_no'] = $ctn[1];
         $fields['user_id'] = $user_id;
-        $fields['expedition_route_id'] = $validated['route_id'];
+//        $fields['expedition_route_id'] = $validated['route_id'];
         CargoExpeditionMovementAction::run($ctn[0], $cargo, $user_id, $ctn[1], rand(4, 10), 1, 'load_cargo_expedition', $expedition->car->plaka);
 
         return LoadCargoToExpeditionAction::run($fields);
